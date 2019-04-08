5ae0c9bcf95120ccef537c7e3d83acbef38eccdf
fernandoms <marinhosilva.fernando@gmail.com>
2019-04-08 13:56:03 +0100

    investor and company builder implemented

---
+++ src/interfaces/CompanyBuilderInterface.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+/**
+ *
+ * @author fernandoms
+ */
+public interface CompanyBuilderInterface {
+    
+    public void setName(String name);
+    public void setNShares(int amount);
+    public void IPOShareValue(int value);
+    
+}
